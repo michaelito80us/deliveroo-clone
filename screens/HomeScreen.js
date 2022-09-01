d75c6c93d721195605cd44cf494e88d06e3975e1
Michael Epelboim <sdrmike@outlook.com>
2022-09-01 17:48:28 +0800

    moved sanity from the project to a separate folder to avoid package.json problems

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import {
   UserIcon,
@@ -9,15 +9,35 @@ import {
 } from "react-native-heroicons/outline";
 import Categories from '../components/Categories';
 import FeaturedRow from '../components/FeaturedRow';
+import sanityClient from '../sanity';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([])
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, [])
+
+  useEffect(() => {
+    // fetch stuff from the backend
+    sanityClient.fetch(`
+      *[_type == "featured"] {
+        ...,
+        restaurants[] -> {
+          ...,
+          dishes[] -> {}
+        }
+      }
+    `).then((data) => {
+      setFeaturedCategories(data)
+    })
+  }, [])
+
+  console.log({featuredCategories});
+
 
   return (
     <SafeAreaView className="pt-5 bg-white">
